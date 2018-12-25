a9cab429328be6b2e01f2aba7d90598dda6c3585
Nikita Kazeev <nikita.kazeev@cern.ch>
2018-12-25 03:09:19 +0000

    Add propoer rejection computation

--- utils.py
+++ utils.py
@@ -1,72 +1,46 @@
 import numpy as np
 from sklearn.metrics import make_scorer
 
-# https://stackoverflow.com/questions/21844024/weighted-percentile-using-numpy
-def weighted_percentile(a, q, w=None):
-    """
-    Calculates percentiles associated with a (possibly weighted) array
-
-    Parameters
-    ----------
-    a : array-like
-        The input array from which to calculate percents
-    q : array-like
-        The percentiles to calculate (0.0 - 100.0)
-    w : array-like, optional
-        The weights to assign to values of a.  Equal weighting if None
-        is specified
-
-    Returns
-    -------
-    values : np.array
-        The values associated with the specified percentiles.  
-    """
-    # Standardize and sort based on values in a
-    q = np.array(q) / 100.0
-    if w is None:
-        w = np.ones(a.size)
-    idx = np.argsort(a)
+def find_threshold_for_efficiency(a, e, w):
+    if e < 0 or e > 1:
+        raise ValueError("Efficiency e must be in [0, 1]")
+    # Decreasing order
+    idx = np.argsort(a)[::-1]
     a_sort = a[idx]
+    if w is None:
+        w = np.ones(a.shape)
     w_sort = w[idx]
-
-    # Get the cumulative sum of weights
     ecdf = np.cumsum(w_sort)
+    if (ecdf[-1]) <= 0:
+        raise ValueError("Total weight is < 0")
 
-    # Find the percentile index positions associated with the percentiles
-    p = q * (w.sum() - 1)
+    target_weight_above_threshold = e * ecdf[-1]
+    enough_passing = ecdf >= target_weight_above_threshold
+    first_suitable = np.argmax(enough_passing)
+    last_unsuitable_inv = np.argmin(enough_passing[::-1])
+    if last_unsuitable_inv == 0:
+        raise ValueError("Bug in code")
+    last_unsuitable_plus = len(a) - last_unsuitable_inv
+    return 0.5*(a_sort[first_suitable] + a_sort[last_unsuitable_plus])
 
-    # Find the bounding indices (both low and high)
-    idx_low = np.searchsorted(ecdf, p, side='right')
-    idx_high = np.searchsorted(ecdf, p + 1, side='right')
-    idx_high[idx_high > ecdf.size - 1] = ecdf.size - 1
 
-    # Calculate the weights 
-    weights_high = p - np.floor(p)
-    weights_low = 1.0 - weights_high
-
-    # Extract the low/high indexes and multiply by the corresponding weights
-    x1 = np.take(a_sort, idx_low) * weights_low
-    x2 = np.take(a_sort, idx_high) * weights_high
-
-    # Return the average
-    return np.add(x1, x2)
-
-def get_rejection_at_efficiency_raw(labels, predictions, weights, quantile):
-    signal_indices = (labels >= 1)
-    background_indices = ~signal_indices
+def get_rejection_at_efficiency_raw(
+        labels, predictions, weights, quantile):
+    signal_mask = (labels >= 1)
+    background_mask = ~signal_mask
     if weights is None:
         signal_weights = None
     else:
-        signal_weights = weights[signal_indices]
-    threshold = weighted_percentile(predictions[signal_indices], 
-                                    [quantile], signal_weights)[0]
-    rejected_indices = (predictions[background_indices] < threshold)
+        signal_weights = weights[signal_mask]
+    threshold = find_threshold_for_efficiency(predictions[signal_mask], 
+                                              quantile, signal_weights)
+    rejected_indices = (predictions[background_mask] < threshold)
     if weights is not None:
-        rejected_background = weights[background_indices][rejected_indices].sum()
-        weights_sum = np.sum(weights[background_indices])
+        rejected_background = weights[background_mask][rejected_indices].sum()
+        weights_sum = np.sum(weights[background_mask])
     else:
         rejected_background = rejected_indices.sum()
-        weights_sum = np.sum(background_indices)
+        weights_sum = np.sum(background_mask)
     return rejected_background, weights_sum         
 
 
@@ -76,5 +50,9 @@ def get_rejection_at_efficiency(labels, predictions, threshold, sample_weight=No
     return rejected_background / weights_sum
 
 
-rejection10_sklearn = make_scorer(
-    get_rejection_at_efficiency, needs_threshold=True, threshold=10)
+def rejection90(labels, predictions, sample_weight=None):
+    return get_rejection_at_efficiency(labels, predictions, 0.9, sample_weight=sample_weight)
+
+
+rejection90_sklearn = make_scorer(
+    get_rejection_at_efficiency, needs_threshold=True, threshold=0.9)
